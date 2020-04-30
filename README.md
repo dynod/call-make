3c091e91767ecca5ed473c3da93bb7b6b6deab81
David Pochet <david.pochet@gmail.com>
2020-04-30 18:05:01 +0200

    Deduce project path and handle env vars
    * project path is now optional; it can be deduced from Github project name
    * add capability to specify env vars (e.g. output folder)

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Github action to call a make target
 To use this action, just declare a step like this in your workflow file:
 ```yaml
 - name: My step
-  uses: dynod/call-make@v1
+  uses: dynod/call-make@1.0.0
   with:
       path: some/path
       target: all
@@ -14,8 +14,11 @@ To use this action, just declare a step like this in your workflow file:
 
 Input parameters are:
 
-### path (mandatory)
-Path to use as **`-C`** option when calling make.
+### path
+Path to use as **`-C`** option when calling make. If not set, the path may be detected from repository name, if dynod devenv tools are present in the workspace.
 
 ### target (mandatory)
 Make target to be called.
+
+### env
+Comma separated list of environment variables to be set before calling make.
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,34 @@ PROJECT_PATH="$1"
 shift
 TARGET="$1"
 shift
+ENVVARS="$1"
+shift
 
 # Just make sure we can write in workspace folder; and fix HOME setting
 sudo chmod a+w .
 export HOME=/home/user
+
+# Prepare some environment
+export CI=1
+export CI_PROJECT=`basename ${GITHUB_REPOSITORY}`
+IS_TAG="$(echo "${GITHUB_REF}" | grep "refs/tags/" || true)"
+REPO_HELPER=tools/devenv/helpers/repo.py
+
+# Compute project path if not provided
+if test -z "${PROJECT_PATH}" -a -e "${REPO_HELPER}"; then
+    if test "${CI_PROJECT}" == "workspace" -a -n "${IS_TAG}"; then
+        # Deduce project from tag name
+        export CI_PROJECT="$(echo ${GITHUB_REF} | sed -e "s|refs/tags/\(.*\)-.*|\1|")"
+    fi
+
+    # Project path will be the detected CI one
+    PROJECT_PATH="$(${REPO_HELPER} -r ${PWD}/.repo -p ${CI_PROJECT})"
+fi
+
+# Prepare env vars
+for VAR in $(echo ${ENVVARS} | sed -e "s|,| |g"); do
+    eval "export $VAR"
+done
 
 # Call make
 make -C "${PROJECT_PATH}" "${TARGET}"
